0b7b6f27a27eb9db5cdcb84883a03727cc34b4e6
Raja Nathan <rnathan@vmware.com>
2021-05-24 13:46:12 -0400

    first commit

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "access_key" {
+  default = "AKIAUHLNNGPYAKZU4JNG"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "us-east-1"
+  }
+
